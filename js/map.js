32784dd013defe6b1d2eece609b7695e3d81d7ce
wayne <wayne@ctkpro.com>
2021-07-27 16:55:00 +0800

    七大區hover效果

--- js/map.js
+++ js/map.js
@@ -126,6 +126,48 @@ let rect = new fabric.Rect({
   height: 20
 });
 
+let title_box = new fabric.Rect({
+  left: 0,
+  top: 0,
+  fill: 'rgba(0,0,0,0.6)',
+  width: 200,
+  height: 30
+})
+let title_box_border = new fabric.Rect({
+  left:0,
+  top:0,
+  fill: 'rgba(255,255,255,0.6)',
+  width: 202,
+  height: 32
+})
+
+let title = new fabric.Text('This is a book',{
+  fontFamily: 'Teko',
+  fontSize: 30,
+  fontWeight: 300,
+  left: 0,
+  top: 0,
+  strokeWidth: 0,
+  fill: 'rgba(255,255,255,0.6)',
+});
+let read_more_box = new fabric.Rect({
+  left: 0,
+  top: 0,
+  fill: 'red',
+  width: 200,
+  height: 30
+});
+let read_more = new fabric.Text('MORE',{
+  fontFamily: 'Teko',
+  fontSize: 30,
+  fontWeight: 300,
+  left: 0,
+  top: 0,
+  strokeWidth: 0,
+  fill: 'white',
+})
+
+
 
 function drawGrid() {
   canvas.clear()
@@ -157,62 +199,111 @@ function drawGrid() {
 drawGrid();
 canvas.add(rect);
 
-let particles = [];
-fabric.loadSVGFromURL('js/map_world.svg', function(objects, options) { 
+
+fetch("/js/map_world.json")
+  .then(response => response.json())
+  .then(json => {
+    for (let i = 1; i < json.length; i++) {
+      let polygon = new fabric.Polygon([
+        json[i].points[0],
+        json[i].points[1],
+        json[i].points[2]
+        ], {
+          left: json[i].left/2,
+          top: json[i].top/2,
+          fill: json[i].fill,
+          scaleX: 0.5,
+          scaleY: 0.5,
+          opacity: 0.6,
+          holder:json[i].fill,
+          role: 'none'
+  
+        }
+      )
+      canvas.add(polygon);
+    }
+  });
+// fabric.loadSVGFromURL('js/map_world.svg', function(objects, options) { 
   // var dollars = fabric.util.groupSVGElements(objects, options);
-  // particles = objects;
   // canvas.add(dollars); 
   // canvas.calcOffset();
   // canvas.renderAll();
-  for (let i = 1; i < objects.length; i++) {
-    let polygon = new fabric.Polygon([
-      objects[i].points[0],
-      objects[i].points[1],
-      objects[i].points[2]
-      ], {
-        left: objects[i].left/2.5,
-        top: objects[i].top/2.5,
-        fill: objects[i].fill,
-        scaleX: 0.4,
-        scaleY: 0.4,
-        holder:objects[i].fill,
-      }
-    )
-    canvas.add(polygon);
+
+  // let json = JSON.stringify(objects);
+  // console.log(json);
+  // for (let i = 1; i < objects.length; i++) {
+  //   let polygon = new fabric.Polygon([
+  //     objects[i].points[0],
+  //     objects[i].points[1],
+  //     objects[i].points[2]
+  //     ], {
+  //       left: objects[i].left/2,
+  //       top: objects[i].top/2,
+  //       fill: objects[i].fill,
+  //       scaleX: 0.5,
+  //       scaleY: 0.5,
+  //       holder:objects[i].fill,
+  //       role: 'none'
+
+  //     }
+  //   )
+  //   canvas.add(polygon);
+  // }
+// });
+const citivas = {
+  castitas:{
+    img: '../img/castitas.jpg',
+    link:''
+  },
+  moderatio:{
+    img: '../img/castitas.jpg',
+    link:''
+  },
+  liberalitas:{
+    img: '../img/castitas.jpg',
+    link:''
+  },
+  industria:{
+    img: '../img/castitas.jpg',
+    link:''
+  },
+  patience:{
+    img: '../img/castitas.jpg',
+    link:''
+  },
+  gratia:{
+    img: '../img/castitas.jpg',
+    link:''
+  },
+  humility:{
+    img: '../img/castitas.jpg',
+    link:''
   }
-
-  // let polygon = new fabric.Polygon([
-  //   objects[1].points[0],
-  //   objects[1].points[1],
-  //   objects[1].points[2]
-  //   ], {
-  //     left: 10,
-  //     top: 10,
-  //     fill: "blue"
-  //   }
-  // )
-  // canvas.add(polygon);
-
-});
-
+}
 fabric.loadSVGFromURL('js/map_seven_sign.svg', function(objects, options) {
-
-  for (let i = 1; i < objects.length; i++) {
+  for (let i = 0; i < objects.length; i++) {
     let polygon = new fabric.Polygon([
       objects[i].points[0],
       objects[i].points[1],
       objects[i].points[2]
       ], {
-        left: objects[i].left/2.5,
-        top: objects[i].top/2.5,
+        left: objects[i].left/2,
+        top: objects[i].top/2,
         fill: objects[i].fill,
-        scaleX: 0.4,
-        scaleY: 0.4,
+        scaleX: 0.5,
+        scaleY: 0.5,
         holder:objects[i].fill,
+        id: objects[i].id,
+        role: 'main',
+        img: citivas[ objects[i].id ].img,
+        link: citivas[ objects[i].id ].link
       }
     )
-    canvas.add(polygon);
+      setTimeout(() => {
+        canvas.add(polygon);
+      }, 200);
   }
+  console.log(objects);
 });
 
 
@@ -248,50 +339,83 @@ const dragInfo = {
   lastX: 0,
   lastY: 0
 }
-canvas.on('mouse:down', (e) => {
-  dragInfo.isDragging = true;
-  canvas.selection = false;
-  dragInfo.lastX = e.e.clientX;
-  dragInfo.lastY = e.e.clientY;
+let img;
+canvas.on('mouse:down', function(opt){
+  this.isDragging = true;
+  this.selection = false;
+  this.lastX = opt.e.clientX;
+  this.lastY = opt.e.clientY;
   
+  console.log(opt.target);
+  
+
 })
 const moveLimit = 280;
-canvas.on('mouse:move', (e) => {
-  if (dragInfo.isDragging) {
-    let canvasScale = canvas.viewportTransform[0];
-    let moveFit = canvasScale * canvasScale * -200;
-    if(canvas.viewportTransform[4]> moveLimit ){
-      canvas.viewportTransform[4] = moveLimit ;
-    }else if(canvas.viewportTransform[4] < -moveLimit * canvasScale ** 2){
-      canvas.viewportTransform[4] = -moveLimit * canvasScale **2;
+canvas.on('mouse:move', function(opt){
+  if (this.isDragging) {
+    // console.log(this.viewportTransform);
+    let vpt = this.viewportTransform;
+
+    if(vpt[4] > moveLimit ){
+      vpt[4] = moveLimit ;
+    }else if(vpt[4] < -moveLimit * vpt[0] ** 2){
+      vpt[4] = -moveLimit * vpt[0] **2;
     }else{
-      canvas.viewportTransform[4] += e.e.clientX - dragInfo.lastX;
+      vpt[4] += opt.e.clientX - this.lastX;
     }
 
-    if(canvas.viewportTransform[5] > moveLimit){
-      canvas.viewportTransform[5] = moveLimit;
-    }else if(canvas.viewportTransform[5] < -moveLimit * canvasScale ** 2){
-      canvas.viewportTransform[5] = -moveLimit * canvasScale ** 2;
+    if(vpt[5] > moveLimit){
+      vpt[5] = moveLimit;
+    }else if(vpt[5] < -moveLimit * vpt[0] ** 2){
+      vpt[5] = -moveLimit * vpt[0] ** 2;
     }else{
-      canvas.viewportTransform[5] += e.e.clientY - dragInfo.lastY;
+      vpt[5] += opt.e.clientY - this.lastY;
     }
-    canvas.requestRenderAll();
-    dragInfo.lastX = e.e.clientX;
-    dragInfo.lastY = e.e.clientY;
+    this.requestRenderAll();
+    this.lastX = opt.e.clientX;
+    this.lastY = opt.e.clientY;
+  
   }
 })
 
 canvas.on('mouse:up', function(opt) {
-  dragInfo.isDragging = false;
-  dragInfo.selection = true;
+  this.isDragging = false;
+  this.selection = true;
+  this.forEachObject(function (object){
+    object.setCoords();
+  });
 })
 
 canvas.on('mouse:over', function(e) {
-  e.target.set('fill', 'green');
+  if(e.target.role !=  'none'){
+    e.target.set('fill', 'red');
+
+    let x = e.target.left < 1000 ? e.target.left + 100 : e.target.left - 220;
+    let y = e.target.top - 10;
+    title_box_border.set({left:x-1, top:y-1});
+    title_box.set({left:x, top:y});
+    title.set({text:e.target.id, left:x+5, top:y});
+    read_more_box.set({left:x, top:y+240});
+    read_more.set({left:x+5, top:y+240});
+    canvas.add(title_box_border, title_box, title, read_more_box, read_more);
+
+    fabric.Image.fromURL(e.target.img, function(oImg) {
+      img = oImg;
+      oImg.set({left:x, top:y+40,width:200,height:190});
+      canvas.add(oImg);
+    });
+    // console.log(t);
+  }
   canvas.renderAll();
 })
 
 canvas.on('mouse:out', function(e) {
-  e.target.set('fill', e.target.holder);
-  canvas.renderAll();
+  if(e.target.role !=  'none'){
+
+    e.target.set('fill', e.target.holder);
+
+    canvas.remove(title_box,title_box_border,title,img, read_more_box, read_more);
+
+    canvas.renderAll();
+  }
 });
